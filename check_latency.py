212a9023c21b405a3d661ddc96f65120187479f1
Rob <rtruxal@gmail.com>
2017-07-12 16:30:06 -0700

    This should work...Can't test it yet tho. Still gonna push it. lol.

--- check_latency.py
+++ check_latency.py
@@ -1,22 +1,27 @@
 from glob import glob
 from subprocess import check_output
-
 from multiprocessing.dummy import Pool
 
 
-class Collector:
+class HighSchoolGuidanceCounselor(object):
     """ Collect thread worker results, provide formatting """
-    souls = []
-    errors = []
+    
+    def __new__(cls, *args, **kwargs):
+        cls.souls = []
+        cls.errors = []
 
-    def show():
+    def __init__(self):
+        super(HighSchoolGuidanceCounselor, self).__init__()
+
+    @classmethod
+    def show(cls):
         """ 
         Sort list of dictionaries by dictionary attribute 'latency' 
         Pretty print top 10 results by server name and latency
         """
 
         listicle = sorted(
-            Collector.souls, key=lambda x: float(x['latency'])
+            cls.souls, key=lambda x: float(x['latency'])
         )
         all_ = [[x['latency'], x['name'].split('.')[0]]
                 for x in listicle][:10]
@@ -24,39 +29,43 @@ class Collector:
         for pair in [all_[x] for x in range(10)]:
             print('{} \t {}'.format(pair[1], pair[0]))
 
-
-""" match tcp servers in the US in ovpn folder"""
-match = 'us'
-all_opvn = glob('./OVPN/' + match + '*' + 'tcp*')
-
-
-def cycle(ovpn):
-    """ 
-    Read opvn file, ping IP in bash one time with 1 second timeout
-    Parse result, append latency to Collector object.
-    """
-    with open(ovpn) as s:
-        x = s.readlines()[13]
-        ip = x.split(' ')[1]
-        arg = 'ping -c 1 -w 1 ' + ip
-        try:
-            ping = check_output(arg.split(' '))
-            delay = str(ping).split('time=')[1].split(' ')[0]
-            Collector.souls.append({
-                'name': ovpn.split('/')[-1],
-                'ip': ip,
-                'latency': delay
-            })
-        except Exception as e:
-            Collector.errors.append(e)
+    @classmethod
+    def cycle(cls, ovpn):
+        """ 
+        Read opvn file, ping IP in bash one time with 1 second timeout
+        Parse result, append latency to HighSchoolGuidanceCounselor object.
+        """
+        with open(ovpn) as s:
+            x = s.readlines()[13]
+            ip = x.split(' ')[1]
+            arg = 'ping -c 1 -w 1 ' + ip
+            try:
+                ping = check_output(arg.split(' '))
+                delay = str(ping).split('time=')[1].split(' ')[0]
+                cls.souls.append({
+                    'name': ovpn.split('/')[-1],
+                    'ip': ip,
+                    'latency': delay
+                })
+            except Exception as e:
+                cls.errors.append(e)
 
 
-def threadPool():
+def threadPool(all_opvn):
     """ DO IT 100 TIMES AT ONCE """
-    for i in Pool(100).imap(cycle, all_opvn):
+    for i in Pool(100).imap(HighSchoolGuidanceCounselor.cycle, all_opvn):
         pass
 
+def main():
+    """ match tcp servers in the US in ovpn folder"""
+    match = 'us'
+    
+    all_opvn = glob('./OVPN/' + match + '*' + 'tcp*')
+    
+    threadPool(all_opvn=all_opvn)
+    
+    """ Show top 10 results """
+    HighSchoolGuidanceCounselor().show()
 
-threadPool()
-""" Show top 10 results """
-Collector.show()
+if __name__ == '__main__':
+    main()
