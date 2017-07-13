3e0bde7139f9170473d69ef9d8b69852f5c38920
Rob <rtruxal@gmail.com>
2017-07-13 12:19:31 -0700

    Mostly just reorganization, and making the singleton class follow PEP.

--- check_latency.py
+++ check_latency.py
@@ -1,44 +1,92 @@
-''' All in one for updating vpn files, pinging vpn, connecting to vpn with credentials file'''
+__doc__ = ''' All in one for updating vpn files, pinging vpn, connecting to vpn with credentials file'''
 from glob import glob
+from os import path
 from multiprocessing.dummy import Pool
-from subprocess import call, check_output, getoutput
-import subprocess
-
-passFile = 'realpass.txt'
+from subprocess import call, getoutput
 
 
-class Collector:
-    """ Collect thread worker results, provide formatting """
+
+class Collector(object):
+    """
+    This is a singleton state-machine class which:
+      - Collects thread worker results & stores them in-memory at runtime.
+      - provides formatting
+
+     """
     souls = []
     errors = []
 
-    def sort_results():
-        listicle = sorted(Collector.souls, key=lambda x: x['latency'])
+    # using pass in the __new__ method prevents the creation of class-instances.
+    # in other words, it forces this class to be a singleton.
+    def __new__(cls, *args, **kwargs):
+        pass
+
+    # this is technically unnecessary.
+    # __init__ is used to set initialization params of instances.
+    # however due to the __new__ overwrite above, creating instances is not currently possible.
+    def __init__(self, *args, **kwargs):
+        super(Collector, self).__init__()
+
+    # making these @classmethods gives them consistent access to class-state.
+    # it also prevents you from having to call `Collector` by name from within itself...which is generally bad.
+    @classmethod
+    def sort_results(cls):
+        listicle = sorted(cls.souls, key=lambda x: x['latency'])
         return listicle
 
-    def show():
+    @classmethod
+    def show(cls):
         """ 
         Sort list of dictionaries by dictionary attribute 'latency' 
         Pretty print top 10 results by server name and latency
         """
 
         all_ = [[x['latency'], x['name'].split('.')[0]]
-                for x in Collector.sort_results()][:10]
+                for x in cls.sort_results()][:10]
         print('Server \t Latency')
         for pair in [all_[x] for x in range(10)]:
             print('{} \t {}'.format(pair[1], pair[0]))
 
-    def chicken_dinner():
+    @classmethod
+    def chicken_dinner(cls):
+        return '/'.join(['OVPN', cls.sort_results()[0]['name']])
 
-        return '/'.join(['OVPN', Collector.sort_results()[0]['name']])
 
+
+def update_servers():
+    """
+    Step 1: [OPTIONAL] update the list of VPN servers to check.
+    ===========================================================
+    """
+    call(['sudo', 'bash', 'update_servers.sh'])
+
+def threadPool():
+    """
+    Step 2: collect latencies and store them inside Collector.
+    ==========================================================
+     - & DO IT 300 TIMES AT ONCE
+    """
+    # for safety
+    servers = get_servers()
+    for i in Pool(300).imap(cycle, servers):
+        pass
+
+
+def get_servers():
+    """
+    Step 2a: match tcp servers in the US in ovpn folder.
+    ====================================================
+    """
+    match = 'us'
+    opvn_targets = glob('./OVPN/' + match + '*' + 'tcp*')
+    assert len(opvn_targets) > 1
+    return opvn_targets
 
 def cycle(ovpn):
-    """ 
-    Read opvn file, ping IP in bash one time with 1 second timeout
-    Parse result, append latency to Collector object.
     """
-
+    Step 2b: Read opvn file, ping IP. Append latency to Collector object.
+    =====================================================================
+    """
     with open(ovpn) as s:
         x = s.readlines()[13]
         ip = x.split(' ')[1]
@@ -52,43 +100,33 @@ def cycle(ovpn):
                 'latency': float(delay)
             })
         except IndexError as e:
-
             Collector.errors.append(e)
 
+def show_top_10():
+    """
+    Step 3: show the top 10 results. Pretty self-explanatory.
+    =========================================================
+    """
+    Collector.show()
+    pass
 
-def threadPool():
-    """ DO IT 100 TIMES AT ONCE """
-    for i in Pool(300).imap(cycle, get_servers()):
-        pass
-
-
-def get_servers():
-    """ match tcp servers in the US in ovpn folder"""
-    match = 'us'
-    opvn_targets = glob('./OVPN/' + match + '*' + 'tcp*')
-    assert len(opvn_targets) > 1
-    return opvn_targets
-
-
-def update_servers():
-    """ Updates the servers """
-    call(['sudo', 'bash', 'update_servers.sh'])
-
-
-def connect_vpn():
-    """ Add credentials to file for fastest ovpn file """
+def connect_vpn(pass_file_pth):
+    """
+    Step 4: Add credentials to file for fastest ovpn file. Connect to VPN.
+    ======================================================================
+    """
 
     print('Connecting to {}...'.format(Collector.chicken_dinner()))
 
     with open(Collector.chicken_dinner()) as ovpn_read:
         if not any(
-            'auth-user-pass ' + passFile in line
+            'auth-user-pass ' + pass_file_pth in line
             for line in ovpn_read.readlines()
         ):
 
             with open(Collector.chicken_dinner(), 'a+') as uName:
                 print('writing credential reference')
-                uName.write('auth-user-pass ' + passFile + '\n')
+                uName.write('auth-user-pass ' + pass_file_pth + '\n')
                 uName.write('dhcp-option DNS 8.8.8.8')
             with open(Collector.chicken_dinner()) as uName:
                 print(str([line for line in uName.readlines()][-2:]))
@@ -100,21 +138,21 @@ def connect_vpn():
     call(start_vpn, shell=True)
 
 
-def top_10():
-    Collector.show()
-
-
-def main(refresh=False, connect=False):
-    #TODO add update servers check 24 hour 
+def main(passfile='realpass.txt',refresh=False, connect=False):
+    assert path.exists(path.realpath(passfile))
+    #TODO add update servers check 24 hour
+    #TODO: DO YOU WANT TO KEEP THIS SCRIPT RUNNING IN THE BACKGROUND ALWAYS OR DO YOU WANT TO MODIFY CRONTAB?
     if refresh:
         update_servers()
 
     threadPool()
-    Collector.show()
+
+    show_top_10()
 
     print(Collector.chicken_dinner())
+
     if connect:
-        connect_vpn()
+        connect_vpn(passfile)
 
-
-main(refresh=False, connect=True)
+if __name__ == '__main__':
+    main(refresh=False, connect=True)
