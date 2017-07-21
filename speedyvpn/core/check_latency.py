bb572a80150be53a61de57bdf6ee62556f0e3331
Zach <zach@livestories.com>
2017-07-21 16:09:21 -0700

    ZACH PROVIDES SELF EFFACING ANWERS TO CODE QUESTIONS

--- speedyvpn/core/check_latency.py
+++ speedyvpn/core/check_latency.py
@@ -39,9 +39,10 @@ def get_servers(ovpn_root_dir=os.getcwd()):
     ====================================================
     """
     match = 'us'
-    opvn_targets = glob(sysencode(ovpn_root_dir + '/OVPN/' + match + '*' + 'tcp*')) #<--What is this?
+    opvn_targets = glob(sysencode(ovpn_root_dir + '/OVPN/' + match + '*' + 'tcp*'))  #<--What is this?
     assert len(opvn_targets) > 1
     return opvn_targets
+
 
 def cycle(ovpn):
     """
@@ -49,20 +50,36 @@ def cycle(ovpn):
     =====================================================================
     """
     with open(ovpn) as s:
-        x = s.readlines()[13] #<--What is this.
+        x = s.readlines()[13]  #<--What is this. 
+        ''' ANSWER: BADMAN TING!!! Nord's server IP is on line 13 of their ovpn files 
+        This is a lazy hack and will break if anythin changes :0, needs to be regex. '''
         ip = x.split(' ')[1]
+
         arg = sysencode('ping -c 3 -w 3.5 ' + ip + ' | grep "mdev = "')
 
         try:
-            ping = getoutput(arg) #<-important.
-            delay = ping.split()[3].split('/')[0] #<--What is this.
-            Collector.souls.append({
-                'name': ovpn.split('/')[-1], #<--What is this.
-                'ip': ip,
-                'latency': float(delay)
-            })
+            ping = getoutput(arg)  #<-important.
+            delay = ping.split()[3].split('/')[0]  #<--What is this.
+            ''' ANSWER: Opaque str maniupulation for opaque bash call:
+            $ ping -c 3 -w 3.5 8.8.8.8 | grep "mdev = "
+            Meaning "ping this IP 3 times, timeout if  t > 3.5 seconds and grab the final line of output (that contains the average t). 
+
+            Typical response: "rtt min/avg/max/mdev = 19.982/20.122/20.267/0.164 ms"
+
+            THEN use this python string parsing to take the 'AVE' number, taking 2nd '/' split of 3rd ' ' split
+            '''
+
+            Collector.souls.append(
+                {
+                    'name': ovpn.split('/')[-1],  #<--What is this. 
+                    'ip': ip,
+                    'latency': float(delay)
+                }
+            )
+            ''' ANSWER: me using split indexing instead of os.path.basename to get the filename. path.basename is better. '''
         except IndexError as e:
             Collector.errors.append(e)
+
 
 def show_top_10():
     """
@@ -71,6 +88,7 @@ def show_top_10():
     """
     Collector.show()
     pass
+
 
 def connect_vpn(pass_file_pth):
     """
@@ -81,10 +99,7 @@ def connect_vpn(pass_file_pth):
     print('Connecting to {}...'.format(Collector.chicken_dinner()))
 
     with open(Collector.chicken_dinner()) as ovpn_read:
-        if not any(
-            'auth-user-pass ' + pass_file_pth in line
-            for line in ovpn_read.readlines()
-        ):
+        if not any('auth-user-pass ' + pass_file_pth in line for line in ovpn_read.readlines()):
 
             with open(Collector.chicken_dinner(), 'ab+') as uName:
                 print('writing credential reference')
@@ -103,17 +118,24 @@ def connect_vpn(pass_file_pth):
 
     # DUDE shell=True?! BAYD BAYD BAYD BAYD BAYD BAYD BAAAAYYAYAYAYAYAYAAYDDDDDD NOT GOOD BAYD.
     # that said, to fix this we'll need to instruct python to make a child process.
+    ''' ANSWER: cool, you'll need to explain this to me '''
     #TODO: create a module which creates & "passes the baton" to a child-process.
     call(start_vpn, shell=True)
 
 
-def main(passfile=None,refresh=False, connect=False):
+def main(passfile=None, refresh=False, connect=False):
 
     if passfile and not path.exists(path.realpath(passfile)):
-        raise IOError('the path to `passfile` does not exist. Check your syntax & ensure your file exists.')
+        raise IOError(
+            'the path to `passfile` does not exist. Check your syntax & ensure your file exists.'
+        )
 
     #TODO add update servers check 24 hour
     #TODO: DO YOU WANT TO KEEP THIS SCRIPT RUNNING IN THE BACKGROUND ALWAYS OR DO YOU WANT TO MODIFY CRONTAB?
+    ''' ANSWER How about a variable along the lines of  'refresh servers if it's been > X hours' 
+        Would rather keep this program contained than fuck with system processes.
+    
+    '''
     if refresh:
         update_servers()
 
@@ -128,6 +150,7 @@ def main(passfile=None,refresh=False, connect=False):
         raise IOError('please pass in a text file in the format: username + newline + password.')
     if connect:
         connect_vpn(passfile)
+
 
 if __name__ == '__main__':
     main(refresh=False, connect=True)
